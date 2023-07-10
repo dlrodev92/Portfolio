bb844338fd4fb19fd233b5eeff5a92a6f383e7c6
DLRO92 <108065549+DLRO92@users.noreply.github.com>
2023-07-10 17:55:14 +0100

    letters done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,18 +6,186 @@ export default function Home() {
 
             </div>
             <div className="col-start-3 col-span-2 row-start-1 row-span-2 bg-violet-500">
-                
+
             </div>
-             <motion.h1 className="text-[100px] font-Jost col-start-1 row-start-3 col-span-4 w-full h-1/2 break-normal"
-                initial={{ x: '10%' }}
-                animate={{ x: '0%' }}
-                transition={{
-                duration: 5,
-                repeat: Infinity,
-                repeatType: 'mirror',
-                ease: 'linear',
-                }}
-                 >David Lopez Rodriguez</motion.h1>
+                 <motion.h1 
+                 className="text-[80px] font-Jost col-start-1 row-start-3 col-span-4 w-full h-1/2 break-normal"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ 
+                        duration: 1, 
+                        delay: 0.5 
+                    }}
+                 >
+                <motion.span
+                    style={{ display: 'inline-block', marginRight: '0.1em' }}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.5, repeat: 6, delay: 0.5}}
+                >
+                    D
+                </motion.span>
+                <motion.span
+                    style={{ display: 'inline-block', marginRight: '0.1em' }}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.5, delay: 0.2 ,repeat: 6 }}
+                >
+                    a
+                </motion.span>
+                <motion.span
+                    style={{ display: 'inline-block', marginRight: '0.1em' }}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.5, delay: 0.4 ,repeat: 6 }}
+                >
+                    v
+                </motion.span>
+                <motion.span
+                    style={{ display: 'inline-block', marginRight: '0.1em' }}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.5, delay: 0.6 ,repeat: 6 }}
+                >
+                    i
+                </motion.span>
+                <motion.span
+                    style={{ display: 'inline-block', marginRight: '0.1em' }}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.5, delay: 1 ,repeat: 6 }}
+                > 
+                d
+                </motion.span>
+                <motion.span
+                    style={{ display: 'inline-block', marginRight: '0.1em' }}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.5, delay: 0.8 ,repeat: 6 }}
+                >
+                    &nbsp;
+                </motion.span>
+                <motion.span
+                    style={{ display: 'inline-block', marginRight: '0.1em' }}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.5, delay: 1.2 ,repeat: 5 }}
+                >
+                    L
+                </motion.span>
+                <motion.span
+                    style={{ display: 'inline-block', marginRight: '0.1em' }}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.5, delay: 1.4 ,repeat: 5 }}
+                >
+                    ó
+                </motion.span>
+                <motion.span
+                    style={{ display: 'inline-block', marginRight: '0.1em' }}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.5, delay: 1.6 ,repeat: 5}}
+                >
+                    p
+                </motion.span>
+                <motion.span
+                    style={{ display: 'inline-block', marginRight: '0.1em' }}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.5, delay: 1.8 ,repeat: 5 }}
+                >
+                    e
+                </motion.span>
+                <motion.span
+                    style={{ display: 'inline-block', marginRight: '0.1em' }}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.5, delay: 2 ,repeat: 5 }}
+                >
+                    z
+                </motion.span>
+                <motion.span
+                    style={{ display: 'inline-block', marginRight: '0.1em' }}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.5, delay: 2.2 ,repeat: 4 }}
+                >
+                    &nbsp;
+                </motion.span>
+                <motion.span
+                    style={{ display: 'inline-block', marginRight: '0.1em' }}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.5, delay: 2.4 ,repeat: 4 }}
+                >
+                    R
+                </motion.span>
+                <motion.span
+                    style={{ display: 'inline-block', marginRight: '0.1em' }}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.5, delay: 2.6 ,repeat: 4 }}
+                >
+                    o
+                </motion.span>
+                <motion.span
+                    style={{ display: 'inline-block', marginRight: '0.1em' }}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.5, delay: 2.8 ,repeat: 4 }}
+                >
+                    d
+                </motion.span>
+                <motion.span
+                    style={{ display: 'inline-block', marginRight: '0.1em' }}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.5, delay: 3 ,repeat: 4 }}
+                >
+                   r
+                </motion.span>
+                <motion.span
+                    style={{ display: 'inline-block', marginRight: '0.1em' }}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.5, delay: 3.2 ,repeat: 3 }}
+                >
+                   í
+                </motion.span>
+                <motion.span
+                    style={{ display: 'inline-block', marginRight: '0.1em' }}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.5, delay: 3.4 ,repeat: 3 }}
+                >
+                    g
+                </motion.span>
+                <motion.span
+                    style={{ display: 'inline-block', marginRight: '0.1em' }}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.5, delay: 3.6 ,repeat: 3 }}
+                >
+                    u
+                </motion.span>
+                <motion.span
+                    style={{ display: 'inline-block', marginRight: '0.1em' }}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.5, delay: 3.8 ,repeat: 3 }}
+                >
+                    e
+                </motion.span>
+                <motion.span
+                    style={{ display: 'inline-block', marginRight: '0.1em' }}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.5, delay: 4 ,repeat: 3 }}
+                >
+                    z
+                </motion.span>
+                </motion.h1>
         </div>
     )
 }
