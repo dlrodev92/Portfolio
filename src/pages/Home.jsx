a0bb8f7f481720c9d42ddbd2c9e5e47af70ac3a7
DLRO92 <108065549+DLRO92@users.noreply.github.com>
2023-07-10 17:26:03 +0100

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,23 @@
 import {motion} from "framer-motion"
 export default function Home() {
     return (
-        <div className="h-[100%] w-full flex flex-row items-center justify-center rounded:[20px] lg:rounded-[50px]">
-        <h1 className='text-4xl font-bold underline w-1/2 h-1/2 bg-blueBackground'>Home</h1>
+        <div className="h-[100%] w-full grid grid-cols-4 grid-rows-3 rounded:[20px] lg:rounded-[50px]">
+            <div className="col-start-1 col-span-2 row-start-1 row-span-2 bg-slate-500">
+
+            </div>
+            <div className="col-start-3 col-span-2 row-start-1 row-span-2 bg-violet-500">
+                
+            </div>
+             <motion.h1 className="text-[100px] font-Jost col-start-1 row-start-3 col-span-4 w-full h-1/2 break-normal"
+                initial={{ x: '10%' }}
+                animate={{ x: '0%' }}
+                transition={{
+                duration: 5,
+                repeat: Infinity,
+                repeatType: 'mirror',
+                ease: 'linear',
+                }}
+                 >David Lopez Rodriguez</motion.h1>
         </div>
     )
 }
