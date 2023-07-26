8f4c10355ae6dd70819d2d29c937902ef243d403
DLRO92 <108065549+DLRO92@users.noreply.github.com>
2023-07-26 16:32:10 +0100

    redirects

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: './index.html', // Update this to your main entry HTML file if needed
-      },
-    },
-  },
-});
+})
